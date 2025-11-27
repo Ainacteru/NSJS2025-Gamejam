cc0f7901325f20a929a3c9cce3f928d40ff5b225
ainacteru <gbenawijr@gmail.com>
2025-11-26 18:22:56 -0600

    rooms, and player improvements

---
+++ Assets/Scripts/rooms/door.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    Collider2D coll;
+
+    public Collider2D collision = null;
+    void Awake()
+    {
+        coll = gameObject.GetComponent<Collider2D>();
+    }
+
+    public void Close()
+    {
+        coll.isTrigger = false;
+    }
+
+    public void Open()
+    {
+        coll.isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        collision = other;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        collision = null;
+    }
+}
